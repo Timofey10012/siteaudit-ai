b504e69212fb9fc72353a69a2096371e1ee67e9e
Timofey10012 <timofejbaulkin@gmail.com>
2026-03-08 13:43:35 +0500

    add siteaudit-ai_backend

---
+++ backend/siteaudit-ai_backend/siteaudit-ai_backend/siteaudit-ai_backend.cpp
@@ -0,0 +1,20 @@
+﻿#include <crow.h>
+#include <iostream>
+
+int main()
+{
+    crow::App<> app;
+
+    CROW_ROUTE(app, "/ping")(
+        []() {
+            crow::json::wvalue respJson;
+            respJson["text"] = "pong";
+            return respJson;
+        });
+
+    std::cout << "started server http://localhost:8080\n";
+
+    app.port(8080).multithreaded().run();
+
+    return 0;
+}
