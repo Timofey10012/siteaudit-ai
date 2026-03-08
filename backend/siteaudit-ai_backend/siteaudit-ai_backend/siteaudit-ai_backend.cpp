ef9f89ea1d1a93f3d6d72d41be6227e05a95cb0d
Timofey10012 <timofejbaulkin@gmail.com>
2026-03-08 22:33:23 +0500

    update backend

--- backend/siteaudit-ai_backend/siteaudit-ai_backend/siteaudit-ai_backend.cpp
+++ backend/siteaudit-ai_backend/siteaudit-ai_backend/siteaudit-ai_backend.cpp
@@ -2,10 +2,18 @@
 #include <crow/middlewares/cors.h>
 #include <iostream>
 
-
 int main()
 {
     crow::App<crow::CORSHandler> app;
+
+    auto& cors = app.get_middleware<crow::CORSHandler>();
+    cors.global()
+        .headers("X-Custom-Header", "Upgrade-Insecure-Requests")
+        .methods("POST"_method, "GET"_method)
+        .prefix("/cors")
+        .origin("example.com")
+        .prefix("/nocors")
+        .ignore();
 
     CROW_ROUTE(app, "/ping").methods(crow::HTTPMethod::Get)(
         []() {
