590f877e1864fd4d9e19251788716c26b73172bc
Timofey10012 <timofejbaulkin@gmail.com>
2026-03-08 21:28:37 +0500

    update backend

--- backend/siteaudit-ai_backend/siteaudit-ai_backend/siteaudit-ai_backend.cpp
+++ backend/siteaudit-ai_backend/siteaudit-ai_backend/siteaudit-ai_backend.cpp
@@ -1,16 +1,11 @@
-﻿#include "cors.h"
-
-#include <crow.h>
+﻿#include <crow.h>
+#include <crow/middlewares/cors.h>
 #include <iostream>
+
 
 int main()
 {
-    crow::App<CORSMiddleware> app;
-
-    CROW_ROUTE(app, "/<path>").methods(crow::HTTPMethod::Options)(
-        [](std::string path) {
-            return crow::response(200);
-        });
+    crow::App<crow::CORSHandler> app;
 
     CROW_ROUTE(app, "/ping").methods(crow::HTTPMethod::Get)(
         []() {
